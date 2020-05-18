a3708eb644c128dbc3678ed27719b78184bf7566
etoak <etoak@etoak.com>
2020-05-18 17:58:31 +0800

    提交springmvc-01-hello

---
+++ springmvc-01-hello/src/main/java/com/etoak/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.etoak.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.HttpRequestHandler;
+
+public class HelloController implements HttpRequestHandler {
+
+	@Override
+	public void handleRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//接受参数
+		String name = request.getParameter("name");
+		System.out.println("param name -" + name);
+		
+		//调用service向request传值
+		request.setAttribute("result", "Hello " + name);
+		
+		//返回页面
+		request.getRequestDispatcher("/hello.jsp").forward(request, response);
+
+	}
+
+}
